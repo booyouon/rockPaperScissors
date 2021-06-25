787e5325aeba5e5d8d27d8f7367bf4a5f94bd699
booyouon <66927111+booyouon@users.noreply.github.com>
2021-06-25 00:18:43 -0700

    Added src folder
    - src folder created.
    - src folder contains index.html, app.css, and index.js files

---
+++ src/index.js
@@ -0,0 +1,87 @@
+const startButton = document.querySelector('#start');
+const rockPaperScissors = ['Rock', 'Paper', 'Scissors'];
+
+startButton.addEventListener('click', () => {
+  //create buttons for rock paper scissors input
+  for (const content of rockPaperScissors) {
+    let newButton = document.createElement('button');
+    newButton.textContent = content;
+    newButton.setAttribute('id', content);
+    document.body.append(newButton);
+  }
+  //removes start button
+  startButton.remove();
+  
+  const rockButton = document.querySelector('#Rock');
+  const paperButton = document.querySelector('#Paper')
+  const scissorsButton = document.querySelector('#Scissors')
+  let i = 0;
+
+  rockButton.addEventListener('click', () => {
+    let opponent = Math.floor(Math.random() * 3); //assign random number 0, 1, or 2
+    
+    if (i !== 0) {
+        document.querySelector('#btnContainer').remove();
+    }
+    i++;
+
+    let div = document.createElement('div');
+    div.setAttribute('id', 'btnContainer');
+    document.body.append(div);
+    
+    createSentence('You chose Rock', div);
+    createSentence('Opponent chose ' + rockPaperScissors[opponent], div);
+    endResults(2,1,opponent, div);
+
+
+  });
+  paperButton.addEventListener('click', () => {
+    let opponent = Math.floor(Math.random() * 3); //assign random number 0, 1, or 2
+        
+    if (i !== 0) {
+        document.querySelector('#btnContainer').remove();
+    }
+    i++;
+    
+    let div = document.createElement('div');
+    div.setAttribute('id', 'btnContainer');
+    document.body.append(div);
+    
+    createSentence('You chose Paper', div);
+    createSentence('Opponent chose ' + rockPaperScissors[opponent], div);
+    endResults(0,2,opponent, div);
+  });
+  scissorsButton.addEventListener('click', () => {
+    let opponent = Math.floor(Math.random() * 3); //assign random number 0, 1, or 2
+        
+    if (i !== 0) {
+        document.querySelector('#btnContainer').remove();
+    }
+    i++;
+    
+    let div = document.createElement('div');
+    div.setAttribute('id', 'btnContainer');
+    document.body.append(div);
+    
+    createSentence('You chose Scissors', div);
+    createSentence('Opponent chose ' + rockPaperScissors[opponent], div);
+    endResults(1,0,opponent, div);
+  });
+  
+})
+
+function createSentence(statement, where) {
+  let p = document.createElement('p');
+  p.textContent = statement + '.';
+  where.append(p);
+}
+
+function endResults(winningCondition, losingCondition, opponent, where) {
+  if (opponent == winningCondition) {
+    createSentence('You won', where);
+  } else if (opponent == losingCondition) {
+    createSentence('You lost', where);
+  } else {
+    createSentence('Tie', where);
+  }
+}
